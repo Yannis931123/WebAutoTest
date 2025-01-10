597b3bdee684ba92353057e8341b3e80360a00b7
Yannis931123 <442940309@qq.com>
2025-01-10 19:56:34 +0800

    20250107

--- common/excel_handle.py
+++ common/excel_handle.py
@@ -1,13 +1,17 @@
 import openpyxl
+
+
 class ExcelHandler:
     def __init__(self, file):
         self.file = file
+
     def open_excel(self, sheet_name):
         """打开Excel、获取sheet"""
         wb = openpyxl.load_workbook(self.file)
         # 获取sheet_name
         sheet = wb[sheet_name]
         return sheet
+
     def get_header(self, sheet_name):
         """获取header(表头)"""
         wb = self.open_excel(sheet_name)
@@ -17,6 +21,7 @@ class ExcelHandler:
             # 将遍历出来的表头字段加入列表
             header.append(i.value)
         return header
+
     def read_excel(self, sheet_name):
         """读取所有数据"""
         sheet = self.open_excel(sheet_name)
@@ -29,17 +34,20 @@ class ExcelHandler:
             for cell in row:
                 row_data.append(cell.value)
                 # 通过zip函数将两个列表合并成字典
-                data_dict = dict(zip(self.get_header(sheet_name),row_data))
+                data_dict = dict(zip(self.get_header(sheet_name), row_data))
             data.append(data_dict)
         return data
+
     @staticmethod
-    def write_excel(file, sheet_name, row, cloumn,data):
+    def write_excel(file, sheet_name, row, cloumn, data):
         """Excel写入数据"""
         wb = openpyxl.load_workbook(file)
         sheet = wb[sheet_name]
         sheet.cell(row, cloumn).value = data
         wb.save(file)
         wb.close()
+
+
 if __name__ == "__main__":
     # 以下为测试代码
     excel = ExcelHandler('../data/cases.xlsx')
--- testcase/import_more.py
+++ testcase/import_more.py
@@ -1,7 +1,7 @@
-import statistics
+import statistics  # 引入statistics模块
 
-print(statistics.median([19, -5, 36]))
-print(statistics.mean([19, -5, 36]))
+print(statistics.median([19, -5, 36])) # statistics是模块名，median是函数名 median是中位数
+print(statistics.mean([19, -5, 36])) # statistics是模块名，mean是函数名 mean是平均值
 # statistics是模块名，median mean是函数名
 # 用模块名.函数名 或者 模块名.变量名 去调用
 
