572f25c3bd5d36e645f5c4e4002557da6f0f7a33
Yannis931123 <442940309@qq.com>
2025-01-23 14:12:06 +0800

    20250123

--- playwright/playwright_item.py
+++ playwright/playwright_item.py
@@ -1,5 +1,25 @@
 from playwright.sync_api import sync_playwright
+"""
+context常用操作
 
+context.pages :获取context所有page对象
+
+context.new_page():生成一个新的page对象
+
+context.close()：关闭context
+
+context.add_cookies()：将cookie添加到此浏览器上下文中。此上下文中的所有页面都将安装这些cookie。
+
+只能传入列表
+
+List[{name: str, value: str, url: Union[str, None], domain: Union[str, None], path: Union[str, None], expires: Union[float, None]
+
+context.clear_cookies()：清除context的cookie
+
+context.grant_permissions()：授予浏览器上下文的指定权限，具体见api
+
+context.clear_permissions()：清除授权
+"""
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
